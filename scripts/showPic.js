47791e9ad3e08c50047e07f4426785158f7a90b7
py-task <2529716798@qq.com>
2017-08-29 19:34:35 +0800

    countBodyChildren

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -3,3 +3,9 @@ function showPic(whichpic){
 	var drinks = document.getElementById("drinks")
 	drinks.setAttribute("src", source)
 }
+function countBodyChildren(){
+    var body_element = document.getElementsByTagName("body")[0];
+    alert(body_element.childNodes.length);
+}
+window.onload = countBodyChildren;
+
